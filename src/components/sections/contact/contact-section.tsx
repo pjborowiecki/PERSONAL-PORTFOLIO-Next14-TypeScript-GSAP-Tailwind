1a1df364d34b8521e0ffa8d1486b3984a27328f3
P J Borowiecki <piotr.borowiecki@yahoo.com>
2023-11-02 23:17:11 +0100

    Adjust contact section styling for tablets and medium devices

--- src/components/sections/contact/contact-section.tsx
+++ src/components/sections/contact/contact-section.tsx
@@ -6,16 +6,26 @@ export function ContactSection() {
     <section
       id="contact-section"
       aria-label="contact section"
-      className="mx-auto w-full max-w-[2560px] pb-[24vw] pt-[12vw]"
+      className="mx-auto grid w-full max-w-[2560px] grid-cols-1 gap-[7.6vw] pb-[24vw] pt-[12vw]"
     >
-      <div className="mx-[2rem] grid h-auto grid-cols-1 gap-[12.8vw]">
-        <div className="flex flex-col gap-[12.8vw]">
+      <div className="mx-[2rem] hidden md:flex">
+        <h2 className="text-[16.2vw] font-black leading-[86%]">
+          Have some
+          <span className="text-[14.8vw]"> great ideas?</span>
+        </h2>
+      </div>
+
+      <div className="mx-[2rem] grid h-auto grid-cols-1 gap-[12.8vw] md:grid-cols-2 md:gap-[4.4vw]">
+        <div className="flex w-full flex-col gap-[12.8vw] md:gap-[3.8vw]">
           <div className="flex flex-col gap-[9.6vw]">
-            <h2 className="text-[16.8vw] font-black leading-[86%] tracking-tight">
+            <h2 className="text-[16.8vw] font-black leading-[86%] tracking-tight md:hidden md:text-[8.9vw]">
               Have some
-              <span className="text-[15.8vw]"> great ideas?</span>
+              <span className="text-[15.8vw] md:text-[8.2vw]">
+                {" "}
+                great ideas?
+              </span>
             </h2>
-            <h3 className="text-[11.9vw] font-semibold leading-[110%] tracking-tight">
+            <h3 className="text-[11.9vw] font-semibold leading-[110%] tracking-tight md:text-[6.1vw]">
               Let&apos;s
               <span className={gradient()}> bring them to life </span>
               <span className="font-bold underline decoration-turquoise-base/20 underline-offset-8">
@@ -24,13 +34,13 @@ export function ContactSection() {
             </h3>
           </div>
 
-          <p className="text-[6.8vw] leading-[150%] text-muted-foreground">
+          <p className="text-[6.8vw] leading-[150%] text-muted-foreground md:text-[3.4vw] md:leading-[120%]">
             I am looking for freelance opportunities or a full time job, either
             remotely or in Kraków, where I live.
           </p>
         </div>
 
-        <div>
+        <div className="flex h-full w-full flex-1">
           <ContactForm />
         </div>
       </div>
